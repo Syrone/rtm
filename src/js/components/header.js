63d512ee2bc2daf20f7fba34ce1a122b54628130
Andrew Vasilchenko <cazonove@gmail.com>
2025-07-08 21:30:26 +0300

    Add SCSS components and mixins for header, icons, and sections
    - Created _header.scss for header styles with fixed positioning and responsive padding.
    - Added _icons.scss for icon styling, including SVG handling.
    - Introduced _section.scss for section layout with responsive adjustments and background handling.
    - Established main.scss to import Bootstrap and custom components.
    - Implemented mixins for font-face and responsive breakpoints in _font-face.scss and _responsive.scss.
    - Added vendor.scss for third-party styles and included normalize.css for consistent styling across browsers.

---
+++ src/js/components/header.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+import { getHeaderHeight } from '../functions/header-height.js'
+import { throttle } from '../functions/throttle.js';
+
+getHeaderHeight()
+$(window).on('resize', throttle(getHeaderHeight));
+
+$(window).on('scroll', function () {
+  if ($(window).scrollTop() > 0) {
+    $('.header').addClass('header--fixed');
+  } else {
+    $('.header').removeClass('header--fixed');
+  }
+});
---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+import Modal from "bootstrap/js/dist/modal.js";
+import Offcanvas from "bootstrap/js/dist/offcanvas.js";
+import Dropdown from "bootstrap/js/dist/dropdown.js";
+import Tab from "bootstrap/js/dist/tab.js";
+import Collapse from "bootstrap/js/dist/collapse.js";
+import Alert from "bootstrap/js/dist/alert.js";
+
+import './_components.js';
+
+window.bootstrap = window.bootstrap || {};
+window.bootstrap.Modal = Modal;
+window.bootstrap.Offcanvas = Offcanvas;
+window.bootstrap.Dropdown = Dropdown;
+window.bootstrap.Tab = Tab;
+window.bootstrap.Collapse = Collapse;
+window.bootstrap.Alert = Alert;
